b92a69748368e639f2259c08eb000795ad15b26b
Martyna <mplutwska@gmail.com>
2021-01-26 19:36:47 +0100

    add animation on click (in future on action)

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const foo = document.querySelector(".winner-animation--disable");
+const foo = document.querySelector(".winner-animation");
 const bar = document.querySelector(".result__flex-container");
 
-const foo2 = document.querySelectorAll(".winner-animation--disable")[1];
+const foo2 = document.querySelectorAll(".winner-animation")[1];
 const bar2 = document.querySelectorAll(".result__flex-container")[1];
+
+const ab = document.querySelector(".result__animated-box");
+
 bar.addEventListener("click", function() {
-   foo.classList.add("winner-animation--enable");
-   foo.classList.remove("winner-animation--disable"); console.log(foo.classList);
+   foo.classList.toggle("winner-animation--visable");
+   ab.style.display = "block";
 });
 // foo.classList.add("winner-animation--enable");
 // foo.classList.remove("winner-animation--disable")
@@ -13,15 +16,39 @@ console.log(foo.classList);
 console.log(bar);
 
 bar2.addEventListener("click", function() {
-   foo2.classList.add("winner-animation--enable");
-   foo2.classList.remove("winner-animation--disable"); console.log(foo2.classList);
+   // foo2.classList.toggle("winner-animation--visable");
+   foo2.style["box-shadow"] = "0px 0px 0px 50px rgba(110, 120, 150, 0.05), 0px 0px 0px 120px  rgb(80, 80, 100, 0.1), 0px 0px 0px 180px rgb(90, 90, 90, 0.1)";
+   ab.style.display = "block";
 });
 // foo.classList.add("winner-animation--enable");
 // foo.classList.remove("winner-animation--disable")
 console.log(foo2.classList);
 console.log(bar2);
 
+let z = document.querySelector(".animated-box__btn");
+let r = document.querySelector(".rules");
+let popup = document.querySelector(".rules-popup");
+let x = document.querySelector(".rules-popup__close-btn");
+let b = document.querySelector(".board");
+let rab = document.querySelector(".result");
 
+z.addEventListener("click", function() {
+   location.reload();
+});
+
+r.addEventListener("click", function() {
+   popup.classList.add("rules-popup--visable");
+
+});
+
+x.addEventListener("click", function() {
+   popup.classList.remove("rules-popup--visable");
+});
+
+b.addEventListener("click", function() {
+   b.style.display = "none";
+   rab.style.display = "flex";
+});
 
 
 // const kamień = document.querySelector(".btn1");
